c61d9e2f9cab89f3bf3633a8582126739dcafbfb
uddeshyasonkar <uddeshyasonkarofficial@gmail.com>
2021-08-12 20:28:57 +0530

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Container, Row, Col, Form, InputGroup } from 'react-bootstrap'
 import { Button } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 const Footer = () => {
 	return (
 		<footer className='footer'>
 			<Container>
-				<Row className='my-5'>
+				<Row className='my-3'>
 					<Col sm={12} md={6} lg={4}>
-						<h4 className='my-3'>Contact Us</h4>
+						<h4 className='my-5' align='center'>
+							Contact Us
+						</h4>
 
 						<h7>
 							<h5 className='my-3'> Address-</h5> 6 Ashok Vihar Colony Khurram
@@ -21,26 +24,59 @@ const Footer = () => {
 							<h5 className='my-3'>Email- </h5>srihanumantnursery@gmail.com
 						</h7>
 					</Col>
-					<Col sm={12} md={6} lg={4}>
-						<h4>Locate Us</h4>
+					<Col sm={12} md={6} lg={4} align='center'>
+						<h4 className='my-5'> Locate Us</h4>
+
+						<iframe
+							src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d629.0612280611923!2d80.96671016409297!3d26.887378527477154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3999573db3d78eb7%3A0xea0f409d9055cb2b!2sSri%20Hanumant%20Nursery!5e0!3m2!1sen!2sin!4v1628779164914!5m2!1sen!2sin'
+							width='300'
+							height='300'
+							allowfullscreen=''
+							loading='lazy'
+						></iframe>
 					</Col>
-					<Col sm={12} md={6} lg={4} className='my-5 py-5'>
+
+					<Col sm={12} md={6} lg={4}>
 						<Row>
-							<Col sm={12} md={6} lg={4} className='my-2'>
-								<Button>
-									<i class='fab fa-facebook-f fa-3x'></i>
-								</Button>
-							</Col>
-							<Col sm={12} md={6} lg={4} className='my-2'>
-								<Button>
-									<i class='fab fa-whatsapp fa-3x'></i>
-								</Button>
-							</Col>
-							<Col sm={12} md={6} lg={4} className='my-2'>
-								<Button>
-									<i class='fab fa-instagram fa-3x'></i>
-								</Button>
-							</Col>
+							<h4 className='my-5' align='center'>
+								Socials
+							</h4>
+
+							<Row>
+								<Col className='my-3' md={6} lg={6} align='center'>
+									<Link>
+										<Col>
+											<i class='fab fa-instagram fa-3x'></i>
+										</Col>
+										<h8>@jadebox_21</h8>
+									</Link>
+								</Col>
+								<Col className='my-3' md={6} lg={6} align='center'>
+									<Link>
+										<Col>
+											<i class='fab fa-facebook-f fa-3x'></i>
+										</Col>
+										<h8>@jadebox_21</h8>
+									</Link>
+								</Col>
+								<Col className='my-3' md={6} lg={6} align='center'>
+									<Link>
+										<Col>
+											<i class='fab fa-whatsapp fa-3x'></i>
+										</Col>
+										<h8>+919140382988</h8>
+									</Link>
+								</Col>
+								<Col className='my-3' md={6} lg={6} align='center'>
+									<Link>
+										<Col>
+											<i class='far fa-envelope fa-3x'></i>
+										</Col>
+
+										<h8>srihanumantnursery@gmail.com</h8>
+									</Link>
+								</Col>
+							</Row>
 						</Row>
 					</Col>
 				</Row>
