30c3bfe30b9810cffb6b58084381a4a76a0331fd
uddeshyasonkar <uddeshyasonkarofficial@gmail.com>
2021-08-18 20:01:08 +0530

    blah

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -157,7 +157,7 @@ const Header = () => {
 								</LinkContainer>
 							)}
 							{userInfo && userInfo.isAdmin && (
-								<NavDropdown title='Admin' id='adminmenu'>
+								<NavDropdown title='Admin' id='adminmenu' className='my-2 mx-3'>
 									<LinkContainer to='/admin/userlist'>
 										<NavDropdown.Item>Users</NavDropdown.Item>
 									</LinkContainer>
